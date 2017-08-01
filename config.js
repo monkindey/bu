3c625d0b7fdb3bd8abc6ae8e88c4afcfda850a13
monkindey <monkindey@163.com>
2017-08-01 17:06:17 +0800

    rename bu conf

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ const path = require('path');
 
 module.exports = {
   POST_COMMIT_FILE: 'post-commit',
-  bundlePath: path.resolve(__dirname, 'bundle.json'),
+  bundlePath: path.resolve(__dirname, 'bu.conf'),
   DEFAULT_HOOKS_PATH: '/usr/local/etc/git/hooks/'
 };
