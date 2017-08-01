40a6512ec2ceb95a81aa4c9d3d53df8930d4ee18
monkindey <monkindey@163.com>
2017-08-01 19:12:03 +0800

    exec local post-commit hook

--- scripts/start.js
+++ scripts/start.js
@@ -1,18 +1,38 @@
 const path = require('path');
+
 const fse = require('fs-extra');
 const chalk = require('chalk');
+const spawn = require('cross-spawn');
 
 const bu = require('../index.js');
 const cwd = process.cwd();
-const { bundlePath } = require('../config');
+const { bundlePath, POST_COMMIT_FILE } = require('../config');
+const { hasHook, joinHooksPath, makeSpawnPify } = require('../util');
 
-fse.readJson(bundlePath).then(json => {
-  let where = json[cwd];
-  if (where) {
-    bu(where).then(res => {
-      console.log(chalk.green(res.msg));
+// First, we should verfiy the cwd whether has the post-commit,
+// and we exec it;
+const hasPostCommitHook = hasHook(POST_COMMIT_FILE);
+
+const spawnPify = makeSpawnPify(spawn);
+
+hasPostCommitHook(cwd)
+  .then(
+    isExist =>
+      isExist
+        ? spawnPify('sh', [joinHooksPath(cwd, POST_COMMIT_FILE)], {
+            stdio: 'inherit'
+          })
+        : Promise.resolve()
+  )
+  .then(() => {
+    fse.readJson(bundlePath).then(json => {
+      let where = json[cwd];
+      if (where) {
+        bu(where).then(res => {
+          console.log(chalk.green(res.msg));
+        });
+      } else {
+        console.log(chalk.red(`Please init your bundle path in ${cwd}`));
+      }
     });
-  } else {
-    console.log(chalk.red(`Please init your bundle path in ${cwd}`));
-  }
-});
+  });
--- util.js
+++ util.js
@@ -1,4 +1,26 @@
+const fse = require('fs-extra');
+const path = require('path');
+
 exports.cmdToSpawn = function(cmd) {
   const cmds = cmd.split(' ');
   return [cmds[0], cmds.slice(1)];
 };
+
+exports.joinHooksPath = (root, type) =>
+  path.resolve(root, `.git/hooks/${type}`);
+
+exports.hasHook = type => root => fse.pathExists(exports.joinHooksPath(root, type));
+
+exports.makeSpawnPify = spawn => (...args) => {
+  return new Promise((resolve, reject) => {
+    const child = spawn.apply(null, args);
+    child.on('close', code => {
+      if (code !== 0) {
+        reject();
+        return;
+      }
+      resolve();
+    });
+  });
+};
+
